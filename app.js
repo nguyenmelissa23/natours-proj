aa7c99857a87d1515712cc371f98e38395b63635
Melissa Nguyen <nguyen.melissa23@gmail.com>
2020-08-14 14:29:56 -0700

    error handling middleware

--- app.js
+++ app.js
@@ -4,16 +4,19 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+const AppError = require('./utils/appError');
+const GlobalErrorHandler = require('./controllers/errorController');
+
 const app = express();
 
-// 1) MIDDLEWARES: 'use'
+// NOTE: 1) MIDDLEWARES: 'use'
 if (process.env.NODE_ENV === 'development'){
 	app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/public/`)); // to serve static file.
 
-//our own middleware: apply to every single request. Order matters!
+// NOTE: our own middleware: apply to every single request. Order matters!
 
 //mounting the router
 app.use('/api/v1/tours', tourRouter);
@@ -21,11 +24,12 @@ app.use('/api/v1/users', userRouter);
 
 // NOTE: Order MATTERS! This needs to go after we check all other routes!
 app.all('*', (req, res, next) => {
-	res.status(404).json({
-		status: 'fail', 
-		message: `Can't find ${req.originalUrl} on this server!`
-	});
-	next();
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+//NOTE: whenever we pass something in the next() function, it would know that it is an error and skips all the other middlewares and go to the error handling middleware.
+
+// NOTE: ERROR HANDLING MIDDLEWARE
+app.use(GlobalErrorHandler);
+
 
 module.exports = app;
