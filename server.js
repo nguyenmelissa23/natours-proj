4f92392f9a1dc05660c5a51409607723bf6c923e
Melissa Nguyen <nguyen.melissa23@gmail.com>
2020-08-14 15:54:25 -0700

    added unhunhandledRejection and uncaughtException handlers

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+process.on('uncaughtException', err => { 
+	console.log(err.name, err.message);
+	console.log('UNCAUGHT EXCEPTION: Shutting Down...');
+	process.exit(1);
+})
+
 dotenv.config({path: './config.env'});
 // console.log(process.env);
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
@@ -11,12 +19,23 @@ mongoose
 		useFindAndModify: false, 
 		useUnifiedTopology: true
 	})
-	.then( conn => { console.log('DB connected successfully');});
+	.then( conn => { console.log('DB connected successfully')});
 
 // 4) START SERVER:
 const app = require('./app');
 const port = 8000 || process.env.PORT ;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`App running at http://localhost:${port}/ ...`)
 });
+
+// HANDLING PROMISE REJECTION ERRORS
+process.on('unhandledRejection', err => {
+	console.log(err.name, err.message);
+	console.log('UNHANDLER REJECTION: Shutting Down...');
+	server.close(() => {
+		process.exit(1);
+	})
+});
+
+//
 
